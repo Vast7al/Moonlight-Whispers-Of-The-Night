1be3b46dd1aedaa0d70adf9829f768b715b35d76
Vast7al (Szymon Świerzbin) <73667837+Vast7al@users.noreply.github.com>
2021-03-21 17:02:19 +0100

    Created neccesary files for C#

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Moonlight_Whispers_Of_The_Night
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            double y = 20;
+            Console.WriteLine("x = {1}; y = {0}", x, y);
+        }
+    }
+}
